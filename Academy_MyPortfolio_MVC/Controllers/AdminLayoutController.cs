0fcbe2d32e044c46c7281e8753ca952c68e6b89c
Melisa Akkuş <melisa.akkus01@gmail.com>
2024-11-07 22:19:17 +0300

    Updates

--- Academy_MyPortfolio_MVC/Controllers/AdminLayoutController.cs
+++ Academy_MyPortfolio_MVC/Controllers/AdminLayoutController.cs
@@ -17,5 +17,30 @@ namespace Academy_MyPortfolio_MVC.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult AdminLayoutSpinner()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminLayoutSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminLayoutNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminLayoutFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminLayoutScripts() 
+        {
+            return PartialView();
+        }
     }
 }
---
+++ Academy_MyPortfolio_MVC/Controllers/EducationController.cs
@@ -0,0 +1,31 @@
+﻿using Academy_MyPortfolio_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Academy_MyPortfolio_MVC.Controllers
+{
+    public class EducationController : Controller
+    {
+        MyPortfolioDbEntities db = new MyPortfolioDbEntities();
+        public ActionResult Index()
+        {
+            var educations = db.TblEducations.ToList();
+            return View(educations);
+        }
+
+        public ActionResult DeleteEducation(int id)
+        {
+            var education = db.TblEducations.Find(id);
+            db.TblEducations.Remove(education);
+
+            var educatio2n = db.TblEducations.FirstOrDefault(x=>x.EducationId == id);
+
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
