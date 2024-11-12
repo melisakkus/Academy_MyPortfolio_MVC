76bdb642a2f0bb9a9a275c5f5295b58806b73136
Melisa Akkuş <melisa.akkus01@gmail.com>
2024-11-12 22:14:03 +0300

    Entering UI

--- Academy_MyPortfolio_MVC/Controllers/EducationController.cs
+++ Academy_MyPortfolio_MVC/Controllers/EducationController.cs
@@ -27,5 +27,43 @@ namespace Academy_MyPortfolio_MVC.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+
+        [HttpGet]
+        public ActionResult CreateEducation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEducation(TblEducation model)
+        {
+            db.TblEducations.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            //return RedirectToAction(nameof(Index)); ile de yazılabilir
+        }
+
+        [HttpGet]
+        public ActionResult UpdateEducation(int id)
+        {
+            var education = db.TblEducations.Find(id);
+            return View(education);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEducation(TblEducation model)
+        {
+            var education = db.TblEducations.Find(model.EducationId);
+            education.SchoolName = model.SchoolName;
+            education.DepartmentName = model.DepartmentName;
+            education.StartDate = model.StartDate;
+            education.EndDate = model.EndDate;
+            education.Description = model.Description;
+            education.Degree = model.Degree;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- Academy_MyPortfolio_MVC/Models/TblProject.cs
+++ Academy_MyPortfolio_MVC/Models/TblProject.cs
@@ -11,26 +11,14 @@ namespace Academy_MyPortfolio_MVC.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class TblProject
     {
         public int ProjectId { get; set; }
-
-        [Required(ErrorMessage ="Proje Adý Boþ Geçilemez")]
         public string Name { get; set; }
-
-        [Required(ErrorMessage = "Proje Url Boþ Geçilemez")]
         public string ImageUrl { get; set; }
-
-        [Required(ErrorMessage = "Proje Açýklama Boþ Geçilemez")]
-        [MaxLength(250,ErrorMessage = "Açýklama bölümü en fazla 250 karakter olmalýdýr.")]
         public string Description { get; set; }
-
-        [Required(ErrorMessage = "Kategori Boþ Geçilemez")]
         public int CategoryId { get; set; }
-
-        [Required(ErrorMessage = "Github Linki Boþ Geçilemez")]
         public string GithubUrl { get; set; }
     
         public virtual TblCategory TblCategory { get; set; }
